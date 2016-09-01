11b3ae7e5fa3f800bfaf7aa1b8289e803a328b71
Patrick Pissurno <contato@patrickpissurno.com.br>
2016-09-01 11:15:30 -0300

    First commit

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform gridParent;
+    private static GameObject gridPrefab;
+    private GridItem[][] grid;
+
+    private const float DIST = .1f;
+
+	// Use this for initialization
+	void Start () {
+        LoadResources();
+        Generate(4, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private static void LoadResources()
+    {
+        if(gridPrefab == null)
+            gridPrefab = Resources.Load<GameObject>("Prefabs/Cube");
+    }
+
+    public void GridPressed(GridItem i)
+    {
+        Debug.Log(i.X + " " + i.Y);
+        i.IsWall = !i.IsWall;
+        i.Invalidate();
+    }
+
+    public void Generate(int width, int height)
+    {
+        grid = new GridItem[width][];
+        for (int i = 0; i < width; i++)
+        {
+            grid[i] = new GridItem[height];
+            for (int j = 0; j < height; j++)
+            {
+                GameObject gridObj = Instantiate(gridPrefab, new Vector3(i * (1 + DIST), 0, -j * (1 + DIST)), Quaternion.identity) as GameObject;
+                gridObj.transform.SetParent(gridParent);
+                grid[i][j] = gridObj.AddComponent<GridItem>();
+                grid[i][j].father = this;
+                grid[i][j].X = i;
+                grid[i][j].Y = j;
+                grid[i][j].IsWall = false;
+                grid[i][j].Invalidate();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridItem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridItem : MonoBehaviour {
+    private static Material Dirt = null;
+    private static Material Wall = null;
+
+    public GridController father;
+    public bool IsWall = false;
+    public int X;
+    public int Y;
+
+    private MeshRenderer renderer;
+
+    void Start()
+    {
+        LoadResources();
+        LoadComponents();
+    }
+
+    public void Invalidate()
+    {
+        LoadResources();
+        LoadComponents();
+        renderer.material = IsWall ? Wall : Dirt;
+    }
+
+    void OnMouseDown()
+    {
+        father.GridPressed(this);
+    }
+
+    void LoadResources()
+    {
+        if (Dirt == null)
+            Dirt = Resources.Load<Material>("Materials/Dirt");
+        if (Wall == null)
+            Wall = Resources.Load<Material>("Materials/Wall");
+    }
+
+    void LoadComponents()
+    {
+        if(renderer == null)
+            renderer = GetComponent<MeshRenderer>();
+    }
+}
