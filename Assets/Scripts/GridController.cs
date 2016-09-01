6690ac64d862f934d261cb949b813374ee373faf
Patrick Pissurno <contato@patrickpissurno.com.br>
2016-09-01 11:21:09 -0300

    Centered Grid

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -14,7 +14,7 @@ public class GridController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         LoadResources();
-        Generate(4, 4);
+        Generate(8, 8);
 	}
 	
 	// Update is called once per frame
@@ -30,20 +30,22 @@ public class GridController : MonoBehaviour {
 
     public void GridPressed(GridItem i)
     {
-        Debug.Log(i.X + " " + i.Y);
         i.IsWall = !i.IsWall;
         i.Invalidate();
     }
 
     public void Generate(int width, int height)
     {
+        Vector3 basePos = gridParent.transform.position;
+        basePos = new Vector3(basePos.x - (width * (1 + DIST) / 2f) + .5f, basePos.y, basePos.z + (height * (1 + DIST) / 2f) - .5f);
+        gridParent.position = basePos;
         grid = new GridItem[width][];
         for (int i = 0; i < width; i++)
         {
             grid[i] = new GridItem[height];
             for (int j = 0; j < height; j++)
             {
-                GameObject gridObj = Instantiate(gridPrefab, new Vector3(i * (1 + DIST), 0, -j * (1 + DIST)), Quaternion.identity) as GameObject;
+                GameObject gridObj = Instantiate(gridPrefab, new Vector3(basePos.x + i * (1 + DIST), basePos.y,basePos.z + (-j * (1 + DIST))), Quaternion.identity) as GameObject;
                 gridObj.transform.SetParent(gridParent);
                 grid[i][j] = gridObj.AddComponent<GridItem>();
                 grid[i][j].father = this;
